1c8620082884ba998195aa2feb87d290174d5271
Marc Roige <marc.roige@estudiants.urv.cat>
2022-12-18 19:36:47 +0100

    bluetooth first aproximation

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -1,16 +1,28 @@
 package com.example.weather;
 
 
-
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -22,16 +34,35 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
-
+import java.util.Set;
+import java.util.UUID;
 
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_ENABLE_BT = 0;
+
     RequestQueue requestQueue;
     TextView textView;
     TextView textView2;
     EditText cityText;
     TextView resultText;
+    Button open;
+    Button close;
+    BluetoothAdapter blueAdapter;
+    TextView luminosity;
+    TextView temperature;
+
+    static final int STATE_LISTENING = 1;
+    static final int STATE_CONNECTING=2;
+    static final int STATE_CONNECTED=3;
+    static final int STATE_CONNECTION_FAILED=4;
+    static final int STATE_MESSAGE_RECEIVED=5;
+    private static final String APP_NAME = "BTChat";
+    private static final UUID MY_UUID = UUID.fromString("8ce255c0-223a-11e0-ac64-0803450c9a66");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,9 +70,134 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        blueAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (!blueAdapter.isEnabled()) {
+            showToast("Encenent blueetoth");
+            Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                startActivityForResult(intent, REQUEST_ENABLE_BT);
+                return;
+            }
+        } else {
+            showToast("Blueethoth ja ences");
+        }
+
         requestQueue = Volley.newRequestQueue(this);
         stringRequest();
 
+
+    }
+
+    public void listenClick(View target) {
+        ServerClass serverClass = new ServerClass();
+        serverClass.start();
+    }
+
+
+    private class ServerClass extends Thread {
+        private BluetoothServerSocket serverSocket;
+
+        @SuppressLint("MissingPermission")
+        public ServerClass() {
+            try {
+                    serverSocket = blueAdapter.listenUsingRfcommWithServiceRecord(APP_NAME, MY_UUID);
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void run()
+        {
+            BluetoothSocket socket=null;
+
+            while (socket==null)
+            {
+                try {
+                    Message message=Message.obtain();
+                    message.what = STATE_CONNECTING;
+                    handler.sendMessage(message);
+
+                    socket=serverSocket.accept();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Message message=Message.obtain();
+                    message.what = STATE_CONNECTION_FAILED;
+                    handler.sendMessage(message);
+                }
+
+                if(socket!=null)
+                {
+                    Message message=Message.obtain();
+                    message.what = STATE_CONNECTED;
+                    handler.sendMessage(message);
+
+                    Receive receive = new Receive(socket);
+                    receive.start();
+
+                    break;
+                }
+            }
+        }
+    }
+
+    private class Receive extends Thread
+    {
+        private final BluetoothSocket bluetoothSocket;
+        private final InputStream inputStream;
+
+        public Receive (BluetoothSocket socket)
+        {
+            bluetoothSocket=socket;
+            InputStream tempIn=null;
+
+            try {
+                tempIn=bluetoothSocket.getInputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            inputStream=tempIn;
+        }
+
+        public void run()
+        {
+            byte[] buffer=new byte[1024];
+            int bytes;
+
+            while (true)
+            {
+                try {
+                    bytes=inputStream.read(buffer);
+                    handler.obtainMessage(STATE_MESSAGE_RECEIVED,bytes,-1,buffer).sendToTarget();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+
+    Handler handler=new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+
+            switch (msg.what)
+            {
+                case STATE_MESSAGE_RECEIVED:
+                    byte[] readBuff= (byte[]) msg.obj;
+                    String tempMsg=new String(readBuff,0,msg.arg1);
+                    luminosity.setText(tempMsg);
+                    break;
+            }
+            return true;
+        }
+    });
+
+    private void showToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 
     private void stringRequest() {
